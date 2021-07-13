72a43a89580a9624b71ca5ee35e18b43df8dfe18
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-13 15:23:05 +0700

    Add basic functions for calculator

---
+++ calc.py
@@ -0,0 +1,15 @@
+#Calculator
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 == 0:
+        return "Invalid input"
+    return n1 / n2
